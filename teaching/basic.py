c8d123548768989b68cac1befcbaaa1e42d5826f
KlemenVovk <klemen.vovk@protonmail.com>
2024-10-14 00:05:34 +0200

    Preparation.

---
+++ teaching/basic.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+MAIN_URL = "https://quotes.toscrape.com/"
+driver = webdriver.Chrome() # <- this line is usually problematic, please please contact me if you can't open and navigate to a page with Selenium
+driver.get(MAIN_URL)
+
+# Let's see if everything works.
+# We should see a new Chrome window open and navigate to the URL.
+# Nice, a bit of a delay so we can see what's happening.
+from time import sleep
+sleep(2)
+
+# Mention CSS selectors (and By.CSS_SELECTOR)
+# https://www.w3schools.com/cssref/css_selectors.php
+from selenium.webdriver.common.by import By
+
+# Let's find all boxes with quotes (all divs that have the class "quote")
+quotes_divs = driver.find_elements(By.CSS_SELECTOR, "div.quote")
+print(f"Found {len(quotes_divs)} quotes on the page.")
+assert len(quotes_divs) > 0, "No quotes found on the page."
+assert len(quotes_divs) == 10, f"Expected 10 quotes divs, found {len(quotes_divs)}."
+
+# Let's print the first element to see what we're working with.
+# print(quotes_divs[0].get_attribute("outerHTML"))
+
+result = []
+for quote_div in quotes_divs:
+    entry = {
+        "quote": quote_div.find_element(By.CSS_SELECTOR, "span.text").text,
+        "by": quote_div.find_element(By.CSS_SELECTOR, "span small.author").text,
+        "tags": [tag.text for tag in quote_div.find_elements(By.CSS_SELECTOR, "div.tags a.tag")]
+    }
+    result.append(entry)
+print(result)
+
+import json
+with open("quotes.json", "w") as f:
+    json.dump(result, f, indent=4)
+
+driver.close()
---
+++ teaching/infinitescrollwait.py
@@ -0,0 +1,4 @@
+# wait
+# scroll by waiting
+# check with diff etc.
+# show api?
